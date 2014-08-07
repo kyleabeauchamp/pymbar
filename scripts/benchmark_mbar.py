aa61b01886b2844839960ec08f5ec24e89581c03
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-08-07 17:04:14 -0400

    Some cleanup of scripts

--- scripts/benchmark_mbar.py
+++ scripts/benchmark_mbar.py
@@ -4,9 +4,8 @@ import pymbar
 from pymbar.testsystems.pymbar_datasets import load_gas_data, load_8proteins_data
 import time
 
-def load_oscillators(n_states):
-    name = "%d oscillators" % n_states
-    n_samples = 250
+def load_oscillators(n_states, n_samples):
+    name = "%dx%d oscillators" % (n_states, n_samples)
     O_k = np.linspace(1, 5, n_states)
     k_k = np.linspace(1, 3, n_states)
     N_k = (np.ones(n_states) * n_samples).astype('int')
@@ -14,9 +13,8 @@ def load_oscillators(n_states):
     x_n, u_kn, N_k_output = test.sample(N_k, mode='u_kn')
     return name, u_kn, N_k_output
 
-def load_exponentials(n_states):
-    name = "%d exponentials" % n_states
-    n_samples = 250
+def load_exponentials(n_states, n_samples):
+    name = "%dx%d exponentials" % (n_states, n_samples)
     rates = np.linspace(1, 3, n_states)
     N_k = (np.ones(n_states) * n_samples).astype('int')
     test = pymbar.testsystems.exponential_distributions.ExponentialTestCase(rates)
@@ -24,12 +22,13 @@ def load_exponentials(n_states):
     return name, u_kn, N_k_output
 
 
-solver_protocol = [dict(method="L-BFGS-B", fast=True), dict(method="L-BFGS-B", fast=True), dict(method="L-BFGS-B"), dict(method="adaptive")]
-#solver_protocol = None
+solver_protocol = None
 mbar_gens = {"new":lambda u_kn, N_k: pymbar.MBAR(u_kn, N_k, solver_protocol=solver_protocol)}
 #mbar_gens = {"old":lambda u_kn, N_k: pymbar.old_mbar.MBAR(u_kn, N_k)}
 #mbar_gens = {"new":lambda u_kn, N_k: pymbar.MBAR(u_kn, N_k), "old":lambda u_kn, N_k: pymbar.old_mbar.MBAR(u_kn, N_k)}
-systems = [lambda : load_exponentials(50), lambda : load_exponentials(150), lambda : load_oscillators(50), lambda : load_oscillators(150), load_gas_data, load_8proteins_data]
+systems = [lambda : load_exponentials(25, 100), lambda : load_exponentials(100, 100), lambda : load_exponentials(250, 250),
+lambda : load_oscillators(25, 100), lambda : load_oscillators(100, 100), lambda : load_oscillators(250, 250),
+load_gas_data, load_8proteins_data]
 
 timedata = []
 for version, mbar_gen in mbar_gens.items():
@@ -40,7 +39,7 @@ for version, mbar_gen in mbar_gens.items():
         wsum = np.linalg.norm(np.exp(mbar.Log_W_nk).sum(0) - 1.0)
         wdot = np.linalg.norm(np.exp(mbar.Log_W_nk).dot(N_k) - 1.0)
         grad_norm = np.linalg.norm(pymbar.mbar_solvers.mbar_gradient(u_kn, N_k, mbar.f_k))
-        obj = pymbar.mbar_solvers.mbar_obj(u_kn, N_k, mbar.f_k)
+        obj = pymbar.mbar_solvers.mbar_objective(u_kn, N_k, mbar.f_k)
         timedata.append([name, version, time.time() - time0, grad_norm, wsum, wdot])
 
 
--- scripts/test_manual_protocol.py
+++ scripts/test_manual_protocol.py
@@ -4,9 +4,8 @@ import pymbar
 from pymbar.testsystems.pymbar_datasets import load_gas_data, load_8proteins_data
 import time
 
-def load_oscillators(n_states):
-    name = "%d oscillators" % n_states
-    n_samples = 250
+def load_oscillators(n_states, n_samples):
+    name = "%dx%d oscillators" % (n_states, n_samples)
     O_k = np.linspace(1, 5, n_states)
     k_k = np.linspace(1, 3, n_states)
     N_k = (np.ones(n_states) * n_samples).astype('int')
@@ -14,9 +13,8 @@ def load_oscillators(n_states):
     x_n, u_kn, N_k_output = test.sample(N_k, mode='u_kn')
     return name, u_kn, N_k_output
 
-def load_exponentials(n_states):
-    name = "%d exponentials" % n_states
-    n_samples = 250
+def load_exponentials(n_states, n_samples):
+    name = "%dx%d exponentials" % (n_states, n_samples)
     rates = np.linspace(1, 3, n_states)
     N_k = (np.ones(n_states) * n_samples).astype('int')
     test = pymbar.testsystems.exponential_distributions.ExponentialTestCase(rates)
@@ -24,8 +22,9 @@ def load_exponentials(n_states):
     return name, u_kn, N_k_output
 
 
-#name, u_kn, N_k = load_oscillators(25)
-name, u_kn, N_k = load_gas_data()
+
+name, u_kn, N_k = load_oscillators(250, 250)
+#name, u_kn, N_k = load_gas_data()
 #name, u_kn, N_k = load_8proteins_data()
 u_kn = u_kn[N_k > 0]
 N_k = N_k[N_k > 0]
@@ -37,19 +36,18 @@ time0 = time.time()
 last_time = time0
 f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, np.zeros(n_states), fast=True, method="L-BFGS-B")
 print(time.time() - last_time)
-print(f_k)
 last_time = time.time()
 f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, f_k, fast=True, method="L-BFGS-B")
 print(time.time() - last_time)
-print(f_k)
+last_time = time.time()
+f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, f_k, fast=True, method="L-BFGS-B", tol=1E-25, options=dict(gtol=1E-25, ftol=1E-25))
+print(time.time() - last_time)
 last_time = time.time()
 f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, f_k, method="L-BFGS-B")
 print(time.time() - last_time)
-print(f_k)
 last_time = time.time()
-f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, f_k, method="adaptive")
+f_k, results = pymbar.mbar_solvers.solve_mbar(u_kn, N_k, f_k, method="hybr", tol=1E-25)
 print(time.time() - last_time)
-print(f_k)
 dt = time.time() - time0
 W = pymbar.mbar_solvers.mbar_W_nk(u_kn, N_k, f_k)
 wsum = np.linalg.norm(W.sum(0) - 1.0)
